25b92008e5c0ee222ac2264da03d4a76c7f19c54
fedimad <36986328+fedimad@users.noreply.github.com>
2023-04-06 20:05:24 +0100

    last commit

---
+++ l10_dz_timbre/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+#
+# Copyright (c) 2022  - feddad.imad@gmail.com
+
+{
+    'name': 'Algérie - Timbre Fiscal avec écriture comptable',
+    'version': '22.1',
+    'category': 'Accounting',
+    'description': """
+This is the module to manage the Fiscal Timbre in Odoo.
+========================================================================
+
+
+.
+
+**Email:** imad.feddad@gmail.com
+""",
+    'author': 'imad.feddad@hotmail.fr, Prodigital',
+    'website': 'https://prodigital.dz/',
+    'depends': ['sale','account','purchase', 'base'],
+    'data': [
+
+	'data/timbre_data.xml',
+    'security/ir.model.access.csv',
+    'data/res.bank.csv',
+	'views/timbre_view.xml',
+    'views/sale_invoice_view.xml',
+    
+    ],
+
+    'images': ['static/description/banner.jpg'],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
