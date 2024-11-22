2b9bb21dd43393bacdac378ce2eb90812ace7f67
fedimad <36986328+fedimad@users.noreply.github.com>
2024-11-22 10:40:39 +0100

    Commit to 16.0

---
+++ stock_inventory_ajustement/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Stock Inventory Adjustment",
+    "version": "16.0.0.0.0",
+    "license": "LGPL-3",
+    "category": "Inventory/Inventory",
+    "summary": "Allows to do an easier follow up of the Inventory Adjustments",
+    "author": "feddad.imad@gmail.Com",
+    "website": "",
+    "depends": ["stock","base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/security.xml",
+        "views/stock_inventory.xml",
+        "views/stock_quant.xml",
+        "views/stock_move_line.xml",
+        "views/res_config_settings_view.xml",
+    ],
+    'images': ['static/description/banner.png'],
+    "installable": True,
+    "application": False,
+}
