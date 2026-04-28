d366f2d8952f7a1d9d82c32d53393a25cac47da0
fedimad <36986328+fedimad@users.noreply.github.com>
2026-04-28 21:37:02 +0100

    Remove LoginTracker class and web_login method
    Removed the LoginTracker class and its web_login method.

--- fet_user_login_detail/models/res_users.py
+++ fet_user_login_detail/models/res_users.py
@@ -18,21 +18,3 @@ class ResUsers(models.Model):
         string="Sessions"
     )
 
-
-
-
-class LoginTracker(Home):
-
-    def web_login(self, redirect=None, **kw):
-        response = super().web_login(redirect=redirect, **kw)
-
-        if request.session.uid:
-            request.session['just_logged_in'] = True
-
-            # store temporary data in session
-            request.session['login_meta'] = {
-                'ip': request.httprequest.remote_addr,
-                'user_agent': request.httprequest.headers.get('User-Agent'),
-            }
-
-        return response
